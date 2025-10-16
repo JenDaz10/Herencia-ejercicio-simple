fb36c342e97450088ae6d8d6111f47b9c1712176
JenDaz10 <150274568+JenDaz10@users.noreply.github.com>
2025-10-16 00:41:17 -0300

    herencialinkedin.cpp

--- herencialinkedin.cpp
+++ herencialinkedin.cpp
@@ -4,32 +4,33 @@ using namespace std;
 
 class Canino{ //Esta es la clase padre o SuperClase
 	protected:
-		string nombre, especie;
+		string especie;
 	public:
-		Canino(string nombre, string especie){
-			this->nombre=nombre;
+		Canino(string especie){
 			this->especie=especie;
 		}
-	string getNombre() {return nombre;}
 	string getEspecie() {return especie;}
 };
 class Perro : public Canino //Esta es la clase hija o SubClase
-{
+{//los perros pertenecen a los caninos, así como los lobos 
 	private:
 		string raza;
+		string nombre;
 	public:
-		Perro(string nombre, string especie, string raza):Canino(nombre,especie)
+		Perro(string especie, string raza, string nombre):Canino(especie)
 		{
 			this->raza=raza;
+			this->nombre=nombre;
 		}
 		string getRaza(){return raza;}
+		string getNombre() {return nombre;}
 };
 
 int main (){
-	Perro *P1=new Perro("Toby", "Perro", "Pastor Belga");
+	Perro *P1=new Perro("Perro", "Pastor Belga", "Toby");
 	
-	cout << "Se ha ingresado a " << P1->getNombre() << endl;
-	cout << "Cuya especie es " << P1->getEspecie() << endl;
-	cout << "Y su raza es " << P1->getRaza() << endl;	
+	cout << "Se ha ingresado la especie " << P1->getEspecie() << endl;
+	cout << "Cuyo raza es " << P1->getRaza() << endl;
+	cout << "Y su nombre es " << P1->getNombre() << endl;	
 } 
 //Yeny Diaz
